54a23b0d7384e991dc1251cf41efd12ffa4dca17
Vally <113294564+Needalife@users.noreply.github.com>
2024-02-29 08:52:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-try:
-    intents = discord.Intents.all()
-    intents.message_content = True
-    bot = commands.Bot(command_prefix='!',intents=intents);
-except Exception as e:
-    print(e)
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!',intents=intents);
     
 @bot.event
 async def on_ready():
