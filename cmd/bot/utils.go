725a0c83f2cfcebecb5c0322153bf07397328bf8
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-03 13:35:04 +0700

    add handleCommand func

--- cmd/bot/utils.go
+++ cmd/bot/utils.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -13,4 +14,25 @@ func open(sess *discordgo.Session) {
 		log.Fatal(err)
 	}
 	fmt.Println("Comet is up!")
+}
+
+func handleCommand(s *discordgo.Session, m *discordgo.MessageCreate, prefix string, registry map[string]func(*discordgo.Session, *discordgo.MessageCreate, []string)) bool {
+	if !strings.HasPrefix(m.Content, prefix) {
+		return false
+	}
+
+	content := strings.TrimPrefix(m.Content, prefix)
+	args := strings.Fields(content)
+	if len(args) == 0 {
+		return false
+	}
+
+	commandName := args[0]
+	
+	if handler, exists := registry[commandName]; exists {
+		handler(s, m, args[1:])
+		return true
+	}
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Unknown command: %s", commandName))
+	return true
 }
