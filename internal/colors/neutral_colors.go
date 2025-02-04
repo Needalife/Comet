345ff26e918a46d96bcf27f714ebb4cb5019516e
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 14:45:48 +0700

    Create neutral_colors.go

---
+++ internal/colors/neutral_colors.go
@@ -0,0 +1,13 @@
+package colors
+
+//neutral and earthy tones
+const (
+    Beige     = 0xF5F5DC
+    Tan       = 0xD2B48C
+    Brown     = 0xA52A2A
+    Olive     = 0x808000
+    Gray      = 0x808080
+    LightGray = 0xD3D3D3
+    DarkBrown = 0x654321
+    Sand      = 0xC2B280
+)
