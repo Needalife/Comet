810da94051127fb0d26d3611a27dbcc1a1f05530
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 17:01:01 +0700

    remove uneed code

--- cmd/bot/utils.go
+++ cmd/bot/utils.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"Comet/internal/colors"
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -25,34 +23,4 @@ func open(sess *discordgo.Session) {
 	fmt.Println(asciiArt)
 }
 
-func handleCommand(
-	s *discordgo.Session, 
-	m *discordgo.MessageCreate, 
-	prefix string, 
-	registry map[string]func(*discordgo.Session, *discordgo.MessageCreate, []string),
-	) bool {
-	if !strings.HasPrefix(m.Content, prefix) {
-		return false
-	}
 
-	fmt.Printf("%v uses '%v'\n", m.Author.GlobalName, m.Content)
-	content := strings.TrimPrefix(m.Content, prefix)
-	args := strings.Fields(content)
-	if len(args) == 0 {
-		return false
-	}
-
-	commandName := args[0]
-
-	if handler, exists := registry[commandName]; exists {
-		handler(s, m, args[1:])
-		return true
-	}
-
-	embed := &discordgo.MessageEmbed{
-		Title: fmt.Sprintf("Unknown command: !%s", commandName),
-		Color: colors.Red,
-	}
-	s.ChannelMessageSendEmbed(m.ChannelID, embed)
-	return true
-}
