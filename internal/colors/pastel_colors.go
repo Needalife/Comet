8c7be7bcea919d5831a80961a47eeab021cb13f7
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 14:45:50 +0700

    Create pastel_colors.go

---
+++ internal/colors/pastel_colors.go
@@ -0,0 +1,13 @@
+package colors
+
+//soft and light colors
+const (
+	PastelPink    = 0xFFD1DC
+    PastelBlue    = 0xAEC6CF
+    PastelGreen   = 0x77DD77
+    PastelYellow  = 0xFDFD96
+    PastelPurple  = 0xCBAACB
+    PastelOrange  = 0xFFB347
+    PastelTeal    = 0xB3E5FC
+    PastelLavender = 0xE6E6FA
+)
