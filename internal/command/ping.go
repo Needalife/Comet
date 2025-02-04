b8cd56520d0b0d7248fb2f379679651c164d64c5
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 13:50:18 +0700

    Update ping.go

--- internal/command/ping.go
+++ internal/command/ping.go
@@ -10,5 +10,9 @@ import (
 func pingCommand(s *discordgo.Session, m *discordgo.MessageCreate, args[] string) {
 	latency := time.Since(m.Timestamp).Milliseconds()
 	response := fmt.Sprintf("pong: %dms!", latency)
-    s.ChannelMessageSend(m.ChannelID, response)
+	embed := &discordgo.MessageEmbed{
+		Title: response,
+		Color: 0x00ff00,
+	}
+	s.ChannelMessageSendEmbed(m.ChannelID, embed)
 }
