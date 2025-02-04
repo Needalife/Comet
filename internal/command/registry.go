4c52e922ce6c3ff2bf831acd7723bc78e469f80a
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 16:59:47 +0700

    migrate to slash commands

--- internal/command/registry.go
+++ internal/command/registry.go
@@ -2,6 +2,14 @@ package command
 
 import "github.com/bwmarrin/discordgo"
 
-var Registry = map[string]func(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+
+var Registry = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	"ping": pingCommand,
+}
+
+var CommandDefinitions = []*discordgo.ApplicationCommand{
+	{
+		Name: "ping",
+		Description: "Replies with pong!",
+	},
 }
