841e53251d9484834deaffaf05836ef39a122f20
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 14:45:44 +0700

    Create basic_colors.go

---
+++ internal/colors/basic_colors.go
@@ -0,0 +1,13 @@
+package colors
+
+const (
+    Red    = 0xFF0000
+    Green  = 0x00FF00
+    Blue   = 0x0000FF
+    Yellow = 0xFFFF00
+    Purple = 0x800080
+    Cyan   = 0x00FFFF
+    White  = 0xFFFFFF
+    Black  = 0x000000
+    Orange = 0xFFA500
+)
