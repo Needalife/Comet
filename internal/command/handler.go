4ccca53b3cb9e83ef3abed6aa9947a05a11e9d6c
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 18:55:23 +0700

    add log

--- internal/command/handler.go
+++ internal/command/handler.go
@@ -1,10 +1,15 @@
 package command
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 func HandleSlashCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if handler, exists := Registry[i.ApplicationCommandData().Name]; exists {
 		handler(s, i)
+		fmt.Printf("%s uses %v\n",i.Member.User.GlobalName, i.ApplicationCommandData().Name)
 	} else {
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
