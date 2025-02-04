1f68316623dcb138da4fd6f972f4076775706df7
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 14:45:47 +0700

    Create dark_colors.go

---
+++ internal/colors/dark_colors.go
@@ -0,0 +1,13 @@
+package colors
+
+//darker tones
+const (
+    DarkRed     = 0x8B0000
+    DarkGreen   = 0x006400
+    DarkBlue    = 0x00008B
+    DarkPurple  = 0x4B0082
+    DarkCyan    = 0x008B8B
+    DarkOrange  = 0xFF8C00
+    DarkGray    = 0xA9A9A9
+    DarkYellow  = 0xB8860B
+)
