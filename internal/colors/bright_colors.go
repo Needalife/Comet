6d70c983fd752e4ac42953692c63cbb98df77054
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-04 14:45:45 +0700

    Create bright_colors.go

---
+++ internal/colors/bright_colors.go
@@ -0,0 +1,12 @@
+package colors
+
+//vibrant, attention-grabbing colors
+const (
+    BrightPink   = 0xFF007F
+    BrightGreen  = 0x66FF00
+    BrightBlue   = 0x009EFF
+    BrightYellow = 0xFFFF33
+    BrightOrange = 0xFF6600
+    BrightCyan   = 0x00FFFF
+    BrightPurple = 0xBF00FF
+)
