126b26208469e2d2e439b3cf0042afbd3aedd9e0
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-05 18:58:24 +0700

    add new command to registry

--- internal/command/registry.go
+++ internal/command/registry.go
@@ -2,14 +2,26 @@ package command
 
 import "github.com/bwmarrin/discordgo"
 
-
-var Registry = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+var Registry = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 	"ping": pingCommand,
+	"cal":  calculationCommand,
 }
 
 var Definitions = []*discordgo.ApplicationCommand{
 	{
-		Name: "ping",
+		Name:        "ping",
 		Description: "Replies with pong!",
+	},
+	{
+		Name:        "cal",
+		Description: "Calculate a mathematical expresison",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "expression",
+				Description: "The mathematical expression to evaluate",
+				Required:    true,
+			},
+		},
 	},
 }
