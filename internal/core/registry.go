4121c48a6966f8e281c82f54580a685a21437a62
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-05 19:54:06 +0700

    put registry map under core dir

--- internal/core/registry.go
+++ internal/core/registry.go
@@ -1,10 +1,14 @@
-package command
+package core
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"Comet/internal/command"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 var Registry = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
-	"ping": pingCommand,
-	"cal":  calculationCommand,
+	"ping": command.PingCommand,
+	"cal":  command.CalculationCommand,
 }
 
 var Definitions = []*discordgo.ApplicationCommand{
