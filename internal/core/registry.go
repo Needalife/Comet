4f1987ecfc8bae4532959fcab757b1d5c6bf5f60
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-08 20:46:15 +0700

    Update registry.go

--- internal/core/registry.go
+++ internal/core/registry.go
@@ -41,13 +41,13 @@ var CommandDefinitions = []*discordgo.ApplicationCommand{
 			},
 			{
 				Type: discordgo.ApplicationCommandOptionString,
-				Name: "from_currency",
+				Name: "from_country",
 				Description: "Country code/name to convert from (e.g., VN/Vietnam)",
 				Required: true,
 			},
 			{
 				Type: discordgo.ApplicationCommandOptionString,
-				Name: "to_currency",
+				Name: "to_country",
 				Description: "Country code/name to convert to (e.g., USA/United States)",
 				Required: true,
 			},
