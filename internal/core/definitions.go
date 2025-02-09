ee8e00753fb4af9ede1b425031fa78c0474ac48c
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-09 14:26:29 +0700

    Create definitions.go

---
+++ internal/core/definitions.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+var CommandDefinitions = []*discordgo.ApplicationCommand{
+	{
+		Name:        "ping",
+		Description: "Replies with pong!",
+	},
+	{
+		Name:        "cal",
+		Description: "Calculate a mathematical expresison",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "expression",
+				Description: "The mathematical expression to evaluate",
+				Required:    true,
+			},
+		},
+	},
+	{
+		Name:        "conv",
+		Description: "Convert money from one currency to another",
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionNumber,
+				Name:        "amount",
+				Description: "Amount of money to convert",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "from_country",
+				Description: "Country code/name to convert from (e.g., VN/Vietnam)",
+				Required:    true,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "to_country",
+				Description: "Country code/name to convert to (e.g., USA/United States)",
+				Required:    true,
+			},
+		},
+	},
+}
