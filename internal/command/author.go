ebd41bc49bf3e0183730fdc3fa3f34911f00540e
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-10 18:49:22 +0700

    function author

---
+++ internal/command/author.go
@@ -0,0 +1,15 @@
+package command
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func AuthorCommand(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "https://github.com/Needalife",
+		},
+	})
+}
+
