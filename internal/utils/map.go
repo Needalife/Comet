6511d54786e0a1e661a612e6c9960f49dc72c911
Vally <113294564+Needalife@users.noreply.github.com>
2025-02-11 18:09:31 +0700

    add Gw2 time

--- internal/utils/map.go
+++ internal/utils/map.go
@@ -324,3 +324,32 @@ var CountryToCurrency = map[string]string{
 	"Zambia":                            "ZMW",
 	"Zimbabwe":                          "ZWL",
 }
+
+var Gw2Times = map[string][]map[string]string{
+	"Tyria": {
+		{"stage": "Day", "start": "9:30", "end": "10:39"},
+		{"stage": "Dusk", "start": "10:40", "end": "10:44"},
+		{"stage": "Night", "start": "10:45", "end": "11:24"},
+		{"stage": "Dawn", "start": "11:25", "end": "11:29"},
+		{"stage": "Day", "start": "11:30", "end": "12:39"},
+		{"stage": "Dusk", "start": "12:40", "end": "12:44"},
+		{"stage": "Night", "start": "12:45", "end": "1:24"},
+		{"stage": "Dawn", "start": "1:25", "end": "1:29"},
+		{"stage": "Day", "start": "1:30", "end": "2:39"},
+		{"stage": "Dusk", "start": "2:40", "end": "2:44"},
+		{"stage": "Night", "start": "2:45", "end": "3:24"},
+		{"stage": "Dawn", "start": "3:25", "end": "3:29"},
+		{"stage": "Day", "start": "3:30", "end": "4:39"},
+		{"stage": "Dusk", "start": "4:40", "end": "4:44"},
+		{"stage": "Night", "start": "4:45", "end": "5:24"},
+		{"stage": "Dawn", "start": "5:25", "end": "5:29"},
+		{"stage": "Day", "start": "5:30", "end": "6:39"},
+		{"stage": "Dusk", "start": "6:40", "end": "6:44"},
+		{"stage": "Night", "start": "6:45", "end": "7:24"},
+		{"stage": "Dawn", "start": "7:25", "end": "7:29"},
+		{"stage": "Day", "start": "7:30", "end": "8:39"},
+		{"stage": "Dusk", "start": "8:40", "end": "8:44"},
+		{"stage": "Night", "start": "8:45", "end": "9:24"},
+		{"stage": "Dawn", "start": "9:25", "end": "9:29"},
+	},
+}
